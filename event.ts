b2a4038d06de32a6bd99fa4832c20f6b7e7d97e4
Bhavana Sahil Shenai <1622bpss@gmail.com>
2017-09-21 14:07:02 +0530

    Create event.ts

---
+++ event.ts
@@ -0,0 +1,16 @@
+//code to add events to calendar
+//
+//
+
+
+var events=[];
+ events.push({
+            title: 'Title for the event',
+            startTime: startTime,
+            endTime: endTime,
+           eventColor: 'green'
+        });
+        
+        
+        
+        return events;
